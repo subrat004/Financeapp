e36b0131f7cf2981dddc33ac232de89d813e4c29
subrat004 <subratdhapola007@gmail.com>
2025-02-08 01:59:52 +0530

    first commit with  auth

---
+++ app/(dashboard)/page.tsx
@@ -0,0 +1,5 @@
+import { Button } from '@/app/components/ui/button';
+
+export default function Home() {
+	return <p>This is an authentic route</p>;
+}
---
+++ app/test/page.tsx
@@ -0,0 +1,9 @@
+const TestPage = () => {
+	return (
+		<div>
+			<h1>This is a test heading</h1>
+		</div>
+	);
+};
+
+export default TestPage;
